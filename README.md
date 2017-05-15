81176eed6e4a7ffd0935d69c2c1c9bf2ce23e5f7
Martin Danielsson <martin@danielsson.de>
2017-05-15 08:43:23 +0200

    Added docker push and tagging with a list of tags. Updated README accordingly.

--- README.md
+++ README.md
@@ -21,7 +21,11 @@ Set the following environment variables:
 Name | Default | Description
 -----|---------|--------------
 `ACS_ENGINE_REPO` | `https://github.com/Azure/acs-engine` | The source repository for the `acs-engine` repository. If you do not specify this, the `HEAD` of the original repository will be used. Override if you have your own fork, or if you want to specify exactly when you want to pull in changes from upstream into your own fork of `acs-engine`
-`ACS_RUNTIME_IMAGE` | -- | The name of the runtime image you want to create; e.g. `registry.yourcompany.io/azure/acs-engine:latest`
+`ACS_RUNTIME_IMAGE` | -- | The name of the runtime image you want to create, **without** the tag; e.g. `registry.yourcompany.io/azure/acs-engine`
+`DOCKER_TAGS` | `latest` | (OPTIONAL) Space-delimited list of docker tags to create and (optionally) push; example: `export DOCKER_TAGS="$(date +%Y%m%d%H%M%S) latest"`
+`DOCKER_REGISTRY_USER` | -- | (OPTIONAL) Docker registry username; specify if you want to automatically push the image to a Docker registry
+`DOCKER_REGISTRY_PASSWORD` | -- | (OPTIONAL) If `DOCKER_REGISTRY_PASSWORD` is specified, you also need to specify this env var
+`DOCKER_REGISTRY` | Docker Hub | (OPTIONAL) If you want to push your image to a custom Docker registry, specify the FQDN of the registry here, e.g. `someregistry.azurecr.io`
 
 Then call the script
 
@@ -29,11 +33,9 @@ Then call the script
 $ ./build-runtime.sh
 ```
 
-Please note that the script does not (yet) push the image to any repository. To do this, do an additional
+If you have specified `DOCKER_REGISTRY_USER` and `DOCKER_REGISTRY_PASSWORD`, the script will log in to either Docker Hub, or the registry you (optionally) specified in `DOCKER_REGISTRY`, and subsequently push the image with the tags you (optionally) specified in `DOCKER_TAGS`.
 
-```
-$ docker push $ACS_RUNTIME_IMAGE
-```
+Note that you may specify multiple tags, space-delimited, in `DOCKER_TAGS`. If you choose not to specify a list of tags, the docker tag `:latest` will be created.
 
 Now you're set and done to use the runtime image in your build pipelines.
 
--- build-runtime.sh
+++ build-runtime.sh
@@ -2,18 +2,42 @@
 
 set -e
 
-if [ -z "$ACS_RUNTIME_IMAGE" ]; then
+trap traperror ERR
+
+function traperror() {
+   echo "ERROR: Something went wrong."
+   if [ -f ./docker_tag.tmp ]; then
+       echo "INFO: Remving temporary image."
+       if ! docker rmi $(cat ./docker_tag.tmp); then
+           echo "ERROR: Could not delete image."
+       fi
+   fi
+   exit 1
+}
+
+if [ -z "${ACS_RUNTIME_IMAGE}" ]; then
   echo "ERROR: Target image name must be specified in env var ACS_RUNTIME_IMAGE prior"
   echo "starting this script."
   echo ""
   echo "Example:"
-  echo "  ACS_RUNTIME_IMAGE=repo.yourcompany.io/azure/acs-engine:latest $0"
+  echo "  ACS_RUNTIME_IMAGE=repo.yourcompany.io/azure/acs-engine $0"
+  echo "  Specify tags to push in env var DOCKER_TAGS as space delimited list, e.g."
+  echo "  DOCKER_TAGS=\"$(date +%Y%m%d%H%M%S) latest\""
   exit 1
 fi
 
-if [ -z "$ACS_ENGINE_REPO" ]; then
+if [[ ${ACS_RUNTIME_IMAGE} == *":"* ]]; then
+  echo "ERROR: The env var ACS_RUNTIME_IMAGE contains a ':'; specify the tag in the env var"
+  echo "  DOCKER_TAGS instead. Leave DOCKER_TAGS empty to build :latest."
+fi
+
+if [ -z "${DOCKER_TAGS}" ]; then
+  export DOCKER_TAGS="latest"
+fi
+
+if [ -z "${ACS_ENGINE_REPO}" ]; then
   export ACS_ENGINE_REPO=https://github.com/Azure/acs-engine
-  echo "WARNING: Env var ACS_ENGINE_REPO is not set, assuming $ACS_ENGINE_REPO"
+  echo "WARNING: Env var ACS_ENGINE_REPO is not set, assuming ${ACS_ENGINE_REPO}"
 fi
 
 if [ -d "./acs-engine" ]; then
@@ -21,7 +45,7 @@ if [ -d "./acs-engine" ]; then
   git pull
   popd
 else
-  git clone $ACS_ENGINE_REPO
+  git clone ${ACS_ENGINE_REPO}
 fi
 
 pushd acs-engine
@@ -43,6 +67,34 @@ popd
 
 cp acs-engine/acs-engine docker
 
+randomId=$(od -vN "16" -An -tx1 /dev/urandom | tr -d " \n")
+
 pushd docker
-docker build -t $ACS_RUNTIME_IMAGE .
+docker build -t ${randomId} .
+for tag in ${DOCKER_TAGS}; do
+  echo "INFO: Tagging as ${ACS_RUNTIME_IMAGE}:${tag}."
+  docker tag ${randomId} ${ACS_RUNTIME_IMAGE}:${tag}
+done
 popd
+
+if [ -z "${DOCKER_REGISTRY_USER}" ] || [ -z "${DOCKER_REGISTRY_PASSWORD}" ]; then
+  echo "INFO: Not pushing image, env vars DOCKER_REGISTRY_USER or DOCKER_REGISTRY_PASSWORD not set."
+else
+  echo "INFO: Logging in to registry ${DOCKER_REGISTRY}..."
+  if [ -z "${DOCKER_REGISTRY}" ]; then
+    echo "INFO: Env var DOCKER_REGISTRY not set, assuming docker hub."
+    docker login -u ${DOCKER_REGISTRY_USER} -p ${DOCKER_REGISTRY_PASSWORD}
+  else
+    docker login -u ${DOCKER_REGISTRY_USER} -p ${DOCKER_REGISTRY_PASSWORD} ${DOCKER_REGISTRY}
+  fi
+
+  for tag in ${DOCKER_TAGS}; do
+    echo "INFO: Pushing image ${ACS_RUNTIME_IMAGE}."
+    docker push ${ACS_RUNTIME_IMAGE}:${tag}
+  done
+fi
+
+echo "INFO: Untagging random image name ${randomId}"
+docker rmi ${randomId}
+
+echo "INFO: Successfully finished."
